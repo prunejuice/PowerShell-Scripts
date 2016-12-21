8e85c7f9b527f4e8ae4fa037f33d9691aa13a9c2
chimbr0 <samjamesbentley@gmail.com>
2016-12-21 13:36:37 +1100

    Update SetvCPUCores.ps1
    Added if statement for

--- SetvCPUCores.ps1
+++ SetvCPUCores.ps1
@@ -1,8 +1,19 @@
 $vmm = "TestVM1"
 $cpu = 7
 
+if ($cpu % 8 -eq 0 -And $cpu -gt 8){
 
-if ($cpu % 4 -eq 0){
+	$corecount = 8
+
+	#The next code line creates the customisation specification
+	#numCPUs is the total number of CPU cores for the VM, numcorespersocket is how many cores each socket will be configured with
+	$spec = new-object -typename VMware.VIM.virtualmachineconfigspec -property @{'numcorespersocket'=$corecount;'numCPUs'=$cpu}
+	#This line reconfigures the VM with the customisation specification $spec
+	(Get-VM $vmm).ExtensionData.ReconfigVM_Task($spec)
+}
+
+
+elseif ($cpu % 4 -eq 0){
 
 	$corecount = 4
 
