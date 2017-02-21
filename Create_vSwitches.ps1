ff45511e3aedb3e987b692504020ba37b3afa85a
chimbr0 <samjamesbentley@gmail.com>
2017-02-21 16:10:30 +1100

    Update Create_vSwitches.ps1

--- Create_vSwitches.ps1
+++ Create_vSwitches.ps1
@@ -1,12 +1,17 @@
 $hosts = "host1","host2"
 
 Foreach ($i in $hosts){
-$vswitch = Get-VirtualSwitch -VMHost $i -Name vSwitch-DMZ
-New-VirtualPortGroup -Name "VLAN226" -VLANID 226 -VirtualSwitch $vswitch
 
-$vswitch = Get-VirtualSwitch -VMHost $i -Name vSwitch-Prod
-New-VirtualPortGroup -Name "VLAN 235 Servers" -VLANID 235 -VirtualSwitch $vswitch
-New-VirtualPortGroup -Name "VLAN 236 Servers" -VLANID 236 -VirtualSwitch $vswitch
-New-VirtualPortGroup -Name "VLAN 237 Servers" -VLANID 237 -VirtualSwitch $vswitch
-New-VirtualPortGroup -Name "VLAN 239 Servers" -VLANID 239 -VirtualSwitch $vswitch
-}
+  New-VirtualSwitch -VMHost $i -Name vSwitch-Prod
+  New-VirtualSwitch -VMHost $i -Name vSwitch-DMZ
+
+  $vswitch = Get-VirtualSwitch -VMHost $i -Name vSwitch-DMZ
+  New-VirtualPortGroup -Name "VLAN226" -VLANID 226 -VirtualSwitch $vswitch
+
+  $vswitch = Get-VirtualSwitch -VMHost $i -Name vSwitch-Prod
+  New-VirtualPortGroup -Name "VLAN 235 Servers" -VLANID 235 -VirtualSwitch $vswitch
+  New-VirtualPortGroup -Name "VLAN 236 Servers" -VLANID 236 -VirtualSwitch $vswitch
+  New-VirtualPortGroup -Name "VLAN 237 Servers" -VLANID 237 -VirtualSwitch $vswitch
+  New-VirtualPortGroup -Name "VLAN 239 Servers" -VLANID 239 -VirtualSwitch $vswitch
+
+  }
