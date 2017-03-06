46b14ab05ce68d43626875198e9e87cd10073198
chimbr0 <samjamesbentley@gmail.com>
2017-03-07 08:37:42 +1100

    Create VMDeletion.ps1

---
+++ VMDeletion.ps1
@@ -0,0 +1,5 @@
+Start-Transcript vmdeletion.txt                                                        #starts transcript 
+add-pssnapin VMware.VimAutomation.Core                                                 #loads powercli module
+connect-viserver vcenter01                                                             #connects to vcenter (using saved credentials)
+Remove-VM -VM virtualmachine1,virtualmachine2 -DeletePermanently -confirm:$false       #deletes vms
+stop-transcript                                                                        #stops transcript
