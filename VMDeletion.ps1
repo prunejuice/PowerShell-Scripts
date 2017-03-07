997b027312f259aca9a47b474d56bb0416e26ef4
chimbr0 <samjamesbentley@gmail.com>
2017-03-08 08:12:31 +1100

    Update VMDeletion.ps1
    Removed transcription

--- VMDeletion.ps1
+++ VMDeletion.ps1
@@ -1,5 +1,3 @@
-Start-Transcript vmdeletion.txt                                                        #starts transcript 
 add-pssnapin VMware.VimAutomation.Core                                                 #loads powercli module
 connect-viserver vcenter01                                                             #connects to vcenter (using saved credentials)
 Remove-VM -VM virtualmachine1,virtualmachine2 -DeletePermanently -confirm:$false       #deletes vms and supresses confirmation prompt
-stop-transcript                                                                        #stops transcript
