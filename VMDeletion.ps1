600309992ff59698112e446485ef618c53e7d344
samjamesb <samjamesbentley@gmail.com>
2017-03-28 10:34:41 +1100

    Update VMDeletion.ps1
    Added notes

--- VMDeletion.ps1
+++ VMDeletion.ps1
@@ -1,3 +1,7 @@
+#This script is useful for scheduling VM deletions
+#Schedule the task using Windows Task Scheduler ensuring the task is run under the same Domain user you connect to vCenter with 
+#It is a prerequisite that you connect to vCenter via PowerCLI and save your credentials in your profile prior to scheduling
+
 add-pssnapin VMware.VimAutomation.Core                                                 #loads powercli module
 connect-viserver vcenter01                                                             #connects to vcenter (using saved credentials)
 Remove-VM -VM virtualmachine1,virtualmachine2 -DeletePermanently -confirm:$false       #deletes vms and supresses confirmation prompt
