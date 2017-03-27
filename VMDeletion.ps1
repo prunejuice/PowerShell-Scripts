89cfeba64d2d20b0feb6297e9f0553f6c425d3f0
samjamesb <samjamesbentley@gmail.com>
2017-03-28 10:46:22 +1100

    Update VMDeletion.ps1
    Added notes about -whatif testing

--- VMDeletion.ps1
+++ VMDeletion.ps1
@@ -1,6 +1,7 @@
 #This script is useful for scheduling VM deletions
 #Schedule the task using Windows Task Scheduler ensuring the task is run under the same Domain user you connect to vCenter with 
 #It is a prerequisite that you connect to vCenter via PowerCLI and save your credentials in your profile prior to scheduling
+#For testing purposes, add the -whatif switch to the Remove-VM command 
 
 add-pssnapin VMware.VimAutomation.Core                                                 #loads powercli module
 connect-viserver vcenter01                                                             #connects to vcenter (using saved credentials)
